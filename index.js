83a15e5db5f8695b7f05375d420440171fd157ce
Your Name <josueishimwe2250.com>
2026-01-06 12:38:53 +0200

    second

---
+++ index.js
@@ -0,0 +1,10 @@
+const http = require("http");
+
+const PORT = process.env.PORT || 3000;
+
+http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("PayView Marketplace is running 🚀");
+}).listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
